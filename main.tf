91d064555be84e3feb5d30235d9a4d0a5bdbf366
saurav mitra <saurav.karate@gmail.com>
2021-07-15 22:56:18 +0800

    dns support

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name  = "${var.prefix}-vpc"
